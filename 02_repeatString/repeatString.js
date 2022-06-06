12eaac8af8e6bddffa434ed1e8384a506a762602
Dejan Simic <dejansimic@gmx.net>
2022-06-06 19:55:16 +0200

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += phrase;
+    }
+    return result;
 };
 
 // Do not edit below this line
