4c523cf7d67f34175c2b86ae430092d444e02d42
Dejan Simic <dejansimic@gmx.net>
2022-06-08 20:40:27 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    //Check for negative or non-number parameters
+    if ((start < 0 || end < 0) || (!Number.isInteger(start) || !Number.isInteger(end))) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    //Swaps parameters if first one is greater than second
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    //Sums up all numbers between range of parameters
+    for (let i = start; i<= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
