6193880307964a8fe8c77a2cd7c882cf404c68e1
Dejan Simic <dejansimic@gmx.net>
2022-06-13 11:32:23 +0200

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //compact
+    if (year % 4 == 0 && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))) {
+        return true;
+    }
+    return false;
 };
+
+/**
+ * if(year % 4 == 0 && year % 100 != 0) {
+ * return true;
+ * }
+ * else if (year % 400 == 0) {
+ * return true;
+ * }
+ * return false;
+ */
 
 // Do not edit below this line
 module.exports = leapYears;
