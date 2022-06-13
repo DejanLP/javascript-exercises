79a550b55932757f45b4591237744d7b6621cad3
Dejan Simic <dejansimic@gmx.net>
2022-06-13 11:35:11 +0200

    remove useless query

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,20 +1,10 @@
 const leapYears = function(year) {
     //compact
-    if (year % 4 == 0 && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))) {
+    if (year % 4 == 0 && (year % 100 != 0 ||  year % 400 == 0)) {
         return true;
     }
     return false;
 };
-
-/**
- * if(year % 4 == 0 && year % 100 != 0) {
- * return true;
- * }
- * else if (year % 400 == 0) {
- * return true;
- * }
- * return false;
- */
 
 // Do not edit below this line
 module.exports = leapYears;
