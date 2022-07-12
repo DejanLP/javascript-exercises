bab252b8bdb9f87ecf6256feff8eaf500dd3bc40
Dejan Simic <dejansimic@gmx.net>
2022-07-12 19:31:36 +0200

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const formatted = input.replace(/[^\w\s]/gi, '').replace(/ /g,'').toLowerCase();;
+    for (let i = 0; i <= formatted.length / 2; i++) {
+        if(formatted.charAt(i) !== formatted.charAt(formatted.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
