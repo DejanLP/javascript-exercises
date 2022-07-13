0f949daae46788e9309b696b878f3862cc430ccd
Dejan Simic <dejansimic@gmx.net>
2022-07-13 09:39:19 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currYear = new Date().getFullYear();
 
-};
+    people.map((person) => {
+        if(!('yearOfDeath' in person)) {
+            person['yearOfDeath'] = currYear;
+        }
+    })
+    people.sort((prev, curr) => {
+        const prevPerson = prev.yearOfDeath - prev.yearOfBirth;
+        const currPerson = curr.yearOfDeath - curr.yearOfBirth;
+        return currPerson > prevPerson ? 1 : -1;
+    })
+    return people[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
